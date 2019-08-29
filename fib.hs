860e088266cd5a70f0a63202d98fe4a5f1ccc6e6
WhiteCat6142 <whitecat6142+git@gmail.com>
2019-08-30 00:51:29 +0900

    Create fib.hs

---
+++ fib.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE Strict #-}
+data SM2=SM2 {-# UNPACK #-} !Int {-# UNPACK #-} !Int {-# UNPACK #-} !Int
+instance Num SM2 where
+    (SM2 a1 b1 c1) * (SM2 a2 b2 c2)=let b=b1*b2 in SM2 (a1*a2+b) (a1*b2+b1*c2) (b+c1*c2)
+
+fromSM2 (SM2 _ b _)=b
+
+fib n=fromSM2 ((SM2 1 1 0)^n)
+
+main=print$fib 80
